b817e50d010d9bd7a8c2e3dfbc48bbd16239f1bf
Geoff Davis <geoffrey.m.davis@gmail.com>
2012-12-21 19:59:14 -0700

    base project

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 PortfolioApp::Application.routes.draw do
-  get "contact/summary"
-
-  get "pdev/summary"
-
-  get "tech_skills/summary"
-  get "projects/summary"
-  match 'projects/summary' => 'projects#summary', :as => :projects
-  match 'tech_skills/summary' => 'tech_skills#summary', :as => :tech_skills
-  match 'pdev/summary' => 'pdev#summary', :as => :pdev
-  match 'contact/summary' => 'contact#summary', :as => :contact
   root :to => "home#index"
+  resources :project do
+    member do
+      get'summary'
+    end
+  end
+  resources :tech_skills do
+    member do
+      get'summary'
+    end
+  end
+  resources :pdev do
+    member do
+      get'summary'
+    end
+  end
+  resources :contact do
+    member do
+      get'summary'
+    end
+  end
 end
+
