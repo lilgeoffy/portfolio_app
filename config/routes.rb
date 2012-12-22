a42342cd2e2c416b7a16f7dc5384ac2c8d240350
Geoff Davis <geoffrey.m.davis@gmail.com>
2012-12-21 22:58:12 -0700

    updates routes for resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,6 @@
 PortfolioApp::Application.routes.draw do
-  get "contact/index"
-
-  get "pdev/index"
-
-  get "tech_skills/index"
-
-  get "projects/index"
-
   root :to => "home#index"
-  resources :projects do
-    member do
-      get'summary'
-    end
-  end
-  resources :tech_skills do
-    member do
-      get'summary'
-    end
-  end
-  resources :pdev do
-    member do
-      get'summary'
-    end
-  end
-  resources :contact do
-    member do
-      get'summary'
-    end
-  end
+  resources :projects, :pdev, :tech_skills, :contact
+
 end
 
